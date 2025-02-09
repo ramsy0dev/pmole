32b0670c336f166a867290e51b4f164fd9c9d214
ramsy0dev <ramsy0dev@gmail.com>
2025-02-08 22:19:16 -0800

    update(test_algo_lzw.py)

--- tests/test_algo_lzw.py
+++ tests/test_algo_lzw.py
@@ -1,5 +1,26 @@
-from pmole.algo import LZW
+# MIT License
 
+# Copyright (c) 2025 ramsy0dev
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from pmole.algo import LZW
 
 def test_algo_lzw() -> None:
     """
@@ -10,13 +31,12 @@ def test_algo_lzw() -> None:
             "world fire cold cold im cold 123 world on fire i love love not love hello"
 
     lzw = LZW()
-
+    
     compressed_data = lzw.compress(
         data=text_data
     )
-
+    
     decompressed_data = lzw.decompress(
         compressed_data=compressed_data
     )
-
-    assert decompressed_data == text_data
+    assert decompressed_data == text_data 
