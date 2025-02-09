8e53dfe71e4a79c341cbf1d2998a52f1ebf3e35c
ramsy0dev <ramsy0dev@gmail.com>
2025-02-08 22:22:02 -0800

    update(cli.py): add a decompress command and a function for creating cache directory

--- pmole/cli.py
+++ pmole/cli.py
@@ -20,52 +20,86 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+__all__ = [
+    "run"
+]
+
+import os
+
 import typer
 
+from loguru import logger
 from pathlib import Path
 
 from .pmole import Pmole
-from pmole.globals import logger
+
+# Globals
+from pmole.globals import (
+    CACHE_DIR,
+    DICTIONARY_CACHE_FILE_PATH,
+    REVERSE_DICTIONARY_CACHE_FILE_PATH,
+)
 
 cli = typer.Typer()
+
+def setup_cli_dir() -> None:
+    """
+    Create directories needed for the cli.
+    """
+    os.makedirs(CACHE_DIR, exist_ok=True)
+    
+    Path(DICTIONARY_CACHE_FILE_PATH).touch()
+    Path(REVERSE_DICTIONARY_CACHE_FILE_PATH).touch()
 
 @cli.command()
 def compress(
     file_path: str = typer.Option(None, "--file-path", help="The file path."),
     directory_path: str = typer.Option(None, "--dir-path", help="The directory path."),
-    threads: int = typer.Option(3, "--threads", help="The number of threads.")
+    threads: int = typer.Option(7, "--threads", help="The number of threads."),
 ):
     """
     Compress a file
     """
     path = file_path if file_path is not None else directory_path
 
+    logger.info(
+        f"Compressing {'file' if file_path is not None else 'directory'} `{path}`..."
+    )
+
     if not Path(path).exists():
         logger.error(f"The provided path '{path}' doesn't exists.")
         exit(1)
-    
+
     if Path(path).is_symlink():
         logger.error(f"Symlinks are not supported.")
         exit(1)
 
     pmole = Pmole()
 
-    pmole.compress(
-        file_path=file_path,
-        directory_path=directory_path,
-        threads=threads
-    )
+    pmole.compress(file_path=file_path, directory_path=directory_path, threads=threads)
 
 @cli.command()
 def decompress(
-    pm_file_path: str = typer.Option(None, "--pm-file-path", help="The compressed file path (.pm)."),
-    threads: int = typer.Option(3, "--threads", help="The number of threads.")
+    pm_file_path: str = typer.Option(
+        None, "--pm-file-path", help="The compressed file path (.pm)."
+    ),
+    threads: int = typer.Option(7, "--threads", help="The number of threads."),
 ):
     """
     Decompress a file
     """
-    pass
+    if not Path(pm_file_path).exists():
+        logger.error(f"The provided path '{pm_file_path}' doesn't exists.")
+        exit(1)
 
+    logger.info(f"Decompressing `{pm_file_path}`...")
+
+    pmole = Pmole()
+
+    pmole.decompress(file_path=pm_file_path, threads=threads)
+
+    logger.info(f"Decompressing is complete.")
 
 def run() -> None:
+    setup_cli_dir()
     cli()
